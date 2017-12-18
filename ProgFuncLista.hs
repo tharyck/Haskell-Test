e2040b0c6dea98e600f2d76e4cd677c1b35e7c01
wagnerMagno <wagner.sales@ccc.ufcg.edu.br>
2017-12-18 13:43:13 -0300

    Criando teste para o order, preOrde, postOrder

--- ProgFuncLista.hs
+++ ProgFuncLista.hs
@@ -296,7 +296,39 @@ testminimuTree5 = TestCase (assertEqual "testminimuTree5" 2  (minimu (tree3))
 testminimuTree6 = TestCase (assertEqual "testminimuTree6" 2  (minimu (tree3))
 testminimuTree7 = TestCase (assertEqual "testminimuTree7" -1  (minimu (tree3))  
 
+-- test order
+testOrderTree1 = TestCase (assertEqual "testOrderTree1" [10]  (order (tree1))
+testOrderTree2 = TestCase (assertEqual "testOrderTree2" [10, 18]  (order (tree2))
+testOrderTree3 = TestCase (assertEqual "testOrderTree3" [2, 10, 18]  (order (tree3))
+testOrderTree4 = TestCase (assertEqual "testOrderTree4" [2, 10, 13, 18]  (order (tree4))
+testOrderTree5 = TestCase (assertEqual "testOrderTree6" [2, 10, 13, 18, 19]  (order (tree5))
+testOrderTree6 = TestCase (assertEqual "testOrderTree6" [2, 8, 10, 13, 18, 19]  (order (tree6))
+testOrderTree7 = TestCase (assertEqual "testOrderTree7" [-1, 2, 8, 10, 13, 18, 19]  (order (tree7))
+testOrderNIL = TestCase (assertEqual "testOrderNIL" []  (order (NIL))
+
+-- test preOrder 
+testPreOrderTree1 = TestCase (assertEqual "testPreOrderTree1" [10]  (preOrder (tree1))
+testPreOrderTree2 = TestCase (assertEqual "testPreOrderTree2" [10, 18]  (preOrder (tree2))
+testPreOrderTree3 = TestCase (assertEqual "testPreOrderTree3" [10, 2, 18]  (preOrder (tree3))
+testPreOrderTree4 = TestCase (assertEqual "testPreOrderTree4" [10, 2, 18, 13]  (preOrder (tree4))
+testPreOrderTree5 = TestCase (assertEqual "testPreOrderTree5" [10, 2, 18, 13, 19]  (preOrder (tree5))
+testPreOrderTree6 = TestCase (assertEqual "testPreOrderTree6" [10, 2, 8, 18, 13, 19]  (preOrder (tree6))
+testPreOrderTree7 = TestCase (assertEqual "testPreOrderTree7" [10, 2, -1, 8, 18, 13, 19]  (preOrder (tree7))
+testPreOrderNIL = TestCase (assertEqual "testPreOrderNIL" []  (preOrder (NIL))
+
+-- test postOrder
+testPostOrderTree1 = TestCase (assertEqual "testPostOrderTree1" [10]  (postOrder (tree1))
+testPostOrderTree2 = TestCase (assertEqual "testPostOrderTree2" [18, 10]  (postOrder (tree2))
+testPostOrderTree3 = TestCase (assertEqual "testPostOrderTree3" [2, 18, 10]  (postOrder (tree3))
+testPostOrderTree4 = TestCase (assertEqual "testPostOrderTree4" [2, 13, 18, 10]  (postOrder (tree4))
+testPostOrderTree5 = TestCase (assertEqual "testPostOrderTree5" [2, 13, 19, 18, 10]  (postOrder (tree5))
+testPostOrderTree6 = TestCase (assertEqual "testPostOrderTree6" [8, 2, 13, 19, 18, 10]  (postOrder (tree6))
+testPostOrderTree7 = TestCase (assertEqual "testPostOrderTree7" [-1, 8, 2, 13, 19, 18, 10]  (postOrder (tree7))
+testPostOrderNIL = TestCase (assertEqual "testPostOrderNIL" []  (postOrder (NIL))
+
+
 -- Termino Testes BST - 
+
 
 
 tests = TestList [testsize1,testsize2,testsize3,testisbst1,testisbst2,
@@ -311,7 +343,11 @@ tests = TestList [testsize1,testsize2,testsize3,testisbst1,testisbst2,
                   testRemove3a, testRemove3atestmaximumTree1, testmaximumTree2, testmaximumTree3,
                   testmaximumTree4, testmaximumTree5, testmaximumTree6, testmaximumTree7, 
                   testminimuTree1, testminimuTree2, testminimuTree3, testminimuTree4, testminimuTree5, 
-                  testminimuTree6, testminimuTree7]
+                  testminimuTree6, testminimuTree7, testOrderTree1, testOrderTree2, testOrderTree3, testOrderTree4,
+                  testOrderTree5, testOrderTree6, testOrderTree7, testOrderNIL, testPreOrderTree1, testPreOrderTree2, testPreOrderTree3,
+                  testPreOrderTree4, testPreOrderTree5, testPreOrderTree6, testPreOrderTree7, testPreOrderNIL, testPostOrderTree1, 
+                  testPostOrderTree2, testPostOrderTree3, testPostOrderTree4, testPostOrderTree5, testPostOrderTree6,
+                  testPostOrderTree7, testPostOrderNIL]
 
 -- Por favor atualizar esta lista
 {- 
@@ -324,7 +360,7 @@ tests = TestList [testsize1,testsize2,testsize3,testisbst1,testisbst2,
   predecessor 
   sucessor
   remove - Igor - vou fazer
-  preOrder
-  postOrder
-  order
+  preOrder - Wagner - OK
+  postOrder - Wagner - OK
+  order - Wagner - Ok
 -}
